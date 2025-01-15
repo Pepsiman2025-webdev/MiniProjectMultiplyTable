2ccbcc3c6ade8b05f41629c6b1cacac8e04a5c2e
Pepsiman2025-webdev <esmaeelzadehrayan12@outlook.com>
2025-01-15 20:52:25 +0330

    First Commit

---
+++ main.cpp
@@ -0,0 +1,408 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+      int input;
+
+      cout << "Type your number between 2 & 99 to see the multiplication table until 500: " << endl;
+      cin >> input;
+
+      // Validate input
+      if (input < 2 || input > 99) {
+            cout << "Invalid input. Please enter a number between 2 and 99." << endl;
+            return 1;
+      }
+
+      // Checking the condition e.g.if input variable was equal to 2 then print numbers from math's Multiplication table etc.
+      if ( input == 2 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      } else if ( input == 3 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      } else if ( input == 4 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      } else if ( input == 5 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      } else if ( input == 6 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      } else if ( input == 7 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      } else if ( input == 8 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if ( input == 9 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if ( input == 10 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if ( input == 11 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if ( input == 12 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 14 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 15 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 16 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 17 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 18 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 19 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 20 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 21 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 22 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 23 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 24 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 25 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 26 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 27 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 28 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 29 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 30 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 31 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 32 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 33 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 34 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 35 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 36 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 37 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 38 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 39 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 40 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 41 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 42 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 43 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 44 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 45 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 46 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 47 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 48 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 49 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 50 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 51 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 52 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 53 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 54 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 55 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 56 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 57 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 58 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 59 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 60 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 61 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 62 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 63 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 64 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 65 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 66 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 67 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 68 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 69 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 70 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 71 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 72 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 73 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 74 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 75 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 76 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 77 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 78 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 79 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 80 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 81 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 82 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 83 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 84 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 85 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 86 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 87 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 88 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 89 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 90 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 91 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 92 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 93 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 94 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 95 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 96 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 97 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 98 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }else if (input == 99 ) {
+            for (int i = 1; i <= 500; i++) {
+                  cout << input << " * " << i << " = " << input * i << endl;
+            }
+      }
+      return 0;
+}
